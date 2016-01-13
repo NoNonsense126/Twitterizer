6a3461dba287e31e9923db2bbaf3a3f49ce4c84f
Wong You Jing <wongyoujing118@gmail.com>
2016-01-13 13:39:44 -0800

    twitterize, char count, and max length

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *charCount;
 
 @end
 
@@ -16,12 +19,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.textView.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)twitterizeButtonTapped:(id)sender {
+    NSString *text = [self.textView.text lowercaseString];
+    NSMutableString *mutableText = [[NSMutableString alloc]init];
+    NSUInteger length = text.length;
+    
+    for (int i = 0; i < length; i++) {
+        if([text characterAtIndex:i] == 'a'){
+            continue;
+        }
+        if([text characterAtIndex:i] == 'e'){
+            continue;
+        }
+        if([text characterAtIndex:i] == 'i'){
+            continue;
+        }
+        if([text characterAtIndex:i] == 'o'){
+            continue;
+        }
+        if([text characterAtIndex:i] == 'u'){
+            continue;
+        }
+        [mutableText appendString:[NSString stringWithFormat:@"%c", [self.textView.text characterAtIndex:i] ]];
+    }
+    
+    self.textView.text = mutableText;
+    
+}
+
+//- (BOOL) textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+//{
+//    if(textView.text.length >= 140){
+//        return NO;
+//    }
+//    else{
+//        return YES;
+//    }
+//}
+
+- (void)textViewDidChange:(UITextView *)textView{
+    if(self.textView.text.length > 140){
+        self.textView.text = [self.textView.text substringToIndex:140];
+    }
+    
+    self.charCount.text = [NSString stringWithFormat:@"Chars typed: %lu", self.textView.text.length ];
 }
 
 @end
