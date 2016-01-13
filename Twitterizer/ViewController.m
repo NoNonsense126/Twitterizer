bce69f8a8807a84e19ada36a55e7f31ed2cae133
Sean Barry <sfbarry1@gmail.com>
2016-01-13 14:30:00 -0800

    complete twitterizer

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -50,15 +50,38 @@
     
 }
 
-//- (BOOL) textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
-//{
-//    if(textView.text.length >= 140){
-//        return NO;
-//    }
-//    else{
-//        return YES;
-//    }
-//}
+
+- (IBAction)hashtagButtonTapped:(id)sender {
+    NSArray *sentence = [self.textView.text componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    sentence = [sentence filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF != ''"]];
+    NSMutableArray *hashtagSentence = [NSMutableArray new];
+
+    NSString *joinedSentence  = [sentence componentsJoinedByString:@" "];
+
+    NSUInteger charsRemaining = 140 - joinedSentence.length;
+
+
+
+    for(int i = 0; i < sentence.count; i++){
+
+        NSString *word = [sentence objectAtIndex:i];
+        NSLog(@"word in sentence is %@", word);
+
+        if (i % 2 == 1 && [word characterAtIndex:0] != '#' && charsRemaining > 0){
+            [hashtagSentence addObject:[NSString stringWithFormat:@"#%@", word]];
+            charsRemaining--;
+            NSLog(@"%lu", charsRemaining);
+        }
+        else{
+            [hashtagSentence addObject:word];
+        }
+    }
+    NSString *joinedHashtagString  = [hashtagSentence componentsJoinedByString:@" "];
+
+    self.textView.text = joinedHashtagString;
+}
+
+
 
 - (void)textViewDidChange:(UITextView *)textView{
     if(self.textView.text.length > 140){
@@ -66,6 +89,32 @@
     }
     
     self.charCount.text = [NSString stringWithFormat:@"Chars typed: %lu", self.textView.text.length ];
+}
+
+- (IBAction)reverseButtonTapped:(id)sender {
+    NSArray *sentence = [self.textView.text componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    sentence = [sentence filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF != ''"]];
+    NSMutableArray *addReverse = [NSMutableArray new];
+
+    for (int i = 0 ; i < sentence.count; i++) {
+        NSString *word = [sentence objectAtIndex:i];
+
+        if ([word characterAtIndex:0] != '#'){
+            NSMutableString *reverse = [NSMutableString stringWithString:@""];
+
+
+            for(int i = 0; i < word.length; i++){
+                [reverse appendFormat:@"%c",[word characterAtIndex:(word.length-1)-i]];
+            }
+
+            [addReverse addObject:reverse];
+        }
+        else{
+            [addReverse addObject:word];
+        }
+    }
+    NSString *joinedReverseString  = [addReverse componentsJoinedByString:@" "];
+    self.textView.text = joinedReverseString;
 }
 
 @end
